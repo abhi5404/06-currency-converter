eaaa7994c0313d31bc35b1d4b43ff787efbd6590
Abhijit Mondal <abhijitamp378@gmail.com>
2025-06-25 14:55:55 +0530

    fix: ensure correct base path in Vite configuration for currency converter app

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/06-currency-converter/', // <-- use your repo name here
 })
